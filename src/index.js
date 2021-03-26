80f283399084054b7b7842a3ddffef21fa1b3fe6
wllmdeemer <wllmdeemer@gmail.com>
2021-03-26 18:31:00 +0200

    optional requirements

--- src/index.js
+++ src/index.js
@@ -14,10 +14,18 @@ Handlebars.registerHelper('inFavorites', function (movieId, options) {
     }
 })
 
+Handlebars.registerHelper('isCurrentView', function (view, options) {
+    if (getLocalStorage('movies-view') === view) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+})
+
 const apiPath = 'https://my-json-server.typicode.com/moviedb-tech/movies/list';
 
-async function getApi(url, componentDataset) {
-    loader(componentDataset);
+async function getApi(url, loaderComponent) {
+    if (loaderComponent) loader(loaderComponent);
     const response = await fetch(url);
     if (!response.ok) {
         throw `${response.status}: ${response.statusText}`;
@@ -46,19 +54,123 @@ function render(data, componentDataset) {
 
 // Router
 function dispatch() {
-    const singleMovieId = location.hash ? location.hash.slice(1) : "";
-    processSingleMovie(singleMovieId);
+    let singleMovieId = location.hash.match(/^#?\/?movie\/([0-9]+)$/);
+    if (singleMovieId) {
+        processSingleMovie(singleMovieId[1]);
+    }
+}
+
+// Movies controls
+function processMoviesControls() {
+    const componentDataset = 'movies-controls';
+    getApi(apiPath, componentDataset).then(apiData => {
+
+        let moviesGenres = {};
+        const currentGenre = getLocalStorage('current-genre');
+        apiData.forEach(movie => {
+            movie.genres.forEach(genre => {
+                moviesGenres[genre.toLowerCase()] = {
+                    name: genre,
+                    isCurrent: genre.toLowerCase() === currentGenre
+                };
+            });
+        });
+
+        const lcMoviesViesName = 'movies-view';
+        const moviesView = getLocalStorage(lcMoviesViesName) ? getLocalStorage(lcMoviesViesName) : setLocalStorage(lcMoviesViesName, 'cards');
+
+        render({
+            moviesGenres,
+            moviesView
+        }, componentDataset);
+
+        filterGenreListener();
+        processMoviesList(apiData);
+    });
+}
+
+function filterGenreListener() {
+    document.querySelector('.js-filterGenres').addEventListener('change', e => {
+        setLocalStorage('current-genre', e.target.value);
+        processMoviesList();
+    });
+}
+
+function filterViewListener() {
+    document.addEventListener('click',function(e){
+        if (e.target && e.target.classList.contains('js-filterView')){
+            e.preventDefault();
+            let currentMoviesView = getLocalStorage('movies-view');
+            const targetView = e.target.dataset.view;
+            if (targetView === currentMoviesView) {
+                return false;
+            } else {
+                toggleMoviesView(currentMoviesView, targetView);
+            }
+        }
+    });
+}
+
+function toggleMoviesView(currentMoviesView, targetView) {
+    const activeViewClass = 'filter-view__item--active';
+    setLocalStorage('movies-view', targetView);
+    document.querySelectorAll('.js-filterView').forEach(button => {
+        if (button.dataset.view !== targetView) {
+            button.classList.remove(activeViewClass);
+        } else {
+            button.classList.add(activeViewClass);
+        }
+    });
+
+    const moviesViewNodes = document.querySelectorAll('[class*="--movies-view--"]');
+    moviesViewNodes.forEach(elem => {
+        let prevClass = '';
+        let newClassPrefix = '';
+        elem.classList.forEach(elemClass => {
+            const matchRegex = elemClass.match(/^(.*--movies-view--).*$/);
+            if (matchRegex) {
+                prevClass = matchRegex[0];
+                newClassPrefix = matchRegex[1];
+            }
+        });
+        elem.classList.remove(prevClass);
+        elem.classList.add(`${newClassPrefix}${targetView}`);
+    });
 }
 
 // Movies list
-function processMoviesList() {
+function processMoviesList(apiData) {
     const componentDataset = 'movies-list';
-    getApi(apiPath, componentDataset).then(data => {
-        render({
-            api: data,
-            favorites: getLocalStorage('favorites')
-        }, componentDataset);
-    });
+    if (apiData) {
+        renderMoviesList(apiData, componentDataset);
+    } else {
+        getApi(apiPath, componentDataset).then(data => {
+            renderMoviesList(data, componentDataset)
+        })
+    }
+}
+
+function renderMoviesList(apiData, componentDataset) {
+    render({
+        api: filterMoviesList(apiData),
+        favorites: getLocalStorage('favorites'),
+        moviesView: getLocalStorage('movies-view')
+    }, componentDataset);
+}
+
+function filterMoviesList(moviesList) {
+    const currentGenre = getLocalStorage('current-genre');
+    if (currentGenre) {
+        let filteredMoviesList = [];
+        moviesList.forEach(movie => {
+            movie.genres.forEach(genre => {
+                if (genre.toLowerCase() === currentGenre) filteredMoviesList.push(movie);
+            });
+        });
+        return filteredMoviesList;
+    } else {
+        return moviesList;
+    }
 }
 
 // Single movie
@@ -66,7 +178,7 @@ function processSingleMovie(singleMovieId) {
     const componentDataset = 'single-movie';
     toggleModal(singleMovieId);
     if (!singleMovieId) {
-        window.location.hash = '';
+        window.location.hash = '/';
     } else {
         getApi(`${apiPath}/${singleMovieId}`, componentDataset).then(data => {
             render({
@@ -153,7 +265,8 @@ window.addEventListener("DOMContentLoaded", function () {
     dispatch();
     window.addEventListener("hashchange", dispatch);
 
-    processMoviesList();
+    processMoviesControls();
+    filterViewListener();
 
     singleMovieListener();
 
